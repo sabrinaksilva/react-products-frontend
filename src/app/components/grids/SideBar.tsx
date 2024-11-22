58fd7da66992669d5ef0070f962b3363cb0416e1
sabrinaksilva <sabrinakappann@gmail.com>
2024-11-22 04:18:41 -0300

    created sidebar

---
+++ src/app/components/grids/SideBar.tsx
@@ -0,0 +1,90 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import LogoutIcon from "@mui/icons-material/Logout";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import { SvgIconProps } from "@mui/material/SvgIcon";
+import Divider from "@mui/material/Divider";
+
+const MenuItem = ({
+  label,
+  icon,
+  subLabel,
+}: {
+  label: string;
+  icon: React.ReactElement<SvgIconProps>;
+  subLabel: string | undefined;
+}) => {
+  return (
+    <ListItem>
+      <ListItemAvatar>
+        <Avatar>{icon}</Avatar>
+      </ListItemAvatar>
+      <ListItemText primary={label} secondary={subLabel} />
+    </ListItem>
+  );
+};
+
+const MenuItems = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100%",
+      }}
+    >
+      <List
+        sx={{
+          width: "100%",
+          bgcolor: "background.paper",
+          flexGrow: 1,
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <MenuItem
+          label="Produtos"
+          icon={<InventoryIcon />}
+          subLabel="Gestão de Produtos"
+        />
+        <Divider />
+        <MenuItem
+          label="Estoque"
+          subLabel="PointOfSaleIcon"
+          icon={<PointOfSaleIcon />}
+        />
+        <Divider />
+        <Box
+          sx={{
+            marginTop: "auto",
+          }}
+        >
+          <MenuItem label="Logout" icon={<LogoutIcon />} subLabel="" />
+        </Box>
+      </List>
+    </Box>
+  );
+};
+
+export const SideBar = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        width: "240px", // Largura fixa para sidebar
+        bgcolor: "background.paper",
+        borderRight: "1px solid #ccc", // Linha de divisão opcional
+      }}
+    >
+      <MenuItems />
+    </Box>
+  );
+};
--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { MainPageWithRoutes } from "../pages/MainPageWithRoutes";
+import { SideBar } from "../components/grids/SideBar";
 
 export const AppRoutes: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ export const AppRoutes: React.FC = () => {
         <Route path="/" element={<MainPageWithRoutes />} />
         <Route path="/stock" element={<MainPageWithRoutes />} />
         <Route path="/products" element={<MainPageWithRoutes />} />
+        <Route path="/sideBar" element={<SideBar />} />
       </Routes>
     </Router>
   );
